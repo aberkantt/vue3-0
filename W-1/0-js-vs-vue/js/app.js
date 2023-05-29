d514a3c0ce195ce338934dd14fce783f64a1dabe
Berkant <85020893+aberkantt@users.noreply.github.com>
2023-05-29 16:48:56 +0300

    Delete W-1 directory

--- W-1/0-js-vs-vue/js/app.js
+++
@@ -1,9 +0,0 @@
-const todoText = document.querySelector("#todoText");
-const addBtn = document.querySelector("#addBtn");
-const todoList = document.querySelector("#todoList");
-//# ile id leri belirttikten sonra .querySelector ile seçtik.
-addBtn.addEventListener("click",()=>{
-    const listItem = document.createElement("li");
-    listItem.textContent=todoText.value;
-    todoList.append(listItem);    //bu son 3 satır ile li oluşturup verileri yazdırdık.
-});
